2acecdfa37823053c7417572b3c9fcc543066709
Mitch Roote <mitch@r00t.ca>
2024-02-05 12:36:17 -0500

    Move indent highlight to file

--- init.lua
+++ init.lua
@@ -31,23 +31,9 @@ require('lazy').setup({
   spec = {
     -- Git related plugins
     'tpope/vim-fugitive',
-    'tpope/vim-rhubarb',
 
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
-
-    -- Useful plugin to show you pending keybinds.
-    { 'folke/which-key.nvim', opts = {} },
-
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    {
-      'lukas-reineke/indent-blankline.nvim',
-      -- See `:help ibl`
-      main = 'ibl',
-      opts = {
-
-      },
-    },
 
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
