749785595a3e48e891124dbaa83c7bec45685a8c
Mitch Roote <mitch.roote@esentire.com>
2024-02-08 09:18:46 -0500

    Update plugins

--- init.lua
+++ init.lua
@@ -38,6 +38,7 @@ require('lazy').setup({
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
+    -- Import all plugins in lua/plugins
     { import = 'plugins' },
   },
   install = {
