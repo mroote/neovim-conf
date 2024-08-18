4649c3d2485c12e323d6e53b92b138d57f041118
Mitch Roote <mitch@r00t.ca>
2024-08-18 18:43:59 -0400

    Add autotags plugin, update plugins

---
+++ lua/plugins/autotag.lua
@@ -0,0 +1,4 @@
+return {
+    'windwp/nvim-ts-autotag',
+    opts = {} -- this is equalent to setup({}) function
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ local ts_configs = {
   'terraform',
   'yaml',
   'json',
+  'html',
 }
 
 return {
