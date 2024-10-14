50a96fed7fba71684ce72678bf94858879ecbebb
Mitch Roote <mitch@r00t.ca>
2024-10-13 22:25:43 -0400

    dont format code on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,11 +20,6 @@ return {
         lua = { 'stylua' },
         python = { 'isort', 'black' },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
     }
   end,
 }
