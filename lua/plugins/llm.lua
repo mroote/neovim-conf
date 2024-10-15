1a4a6e1d2dc5696a4ef70c63a28fa79f8f9ee69b
Mitch Roote <mitch@r00t.ca>
2024-10-15 19:24:44 -0400

    Add groq llm provider

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -4,7 +4,38 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    -- add any opts here
+    provider = "groq",
+    vendors = {
+      ---@type AvanteProvider
+      groq = {
+        endpoint = "https://api.groq.com/openai/v1/chat/completions",
+        model = "llama-3.2-90b-text-preview",
+        api_key_name = "GROQ_API_KEY",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint,
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+              ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
+            },
+            body = {
+              model = opts.model,
+              messages = { -- you can make your own message, but this is very advanced
+                { role = "system", content = code_opts.system_prompt },
+                { role = "user", content = require("avante.providers.openai").get_user_message(code_opts) },
+              },
+              temperature = 0,
+              max_tokens = 4096,
+              stream = true, -- this will be set by default.
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    }
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
