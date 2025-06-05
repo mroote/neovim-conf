d85f0cb77a1af2ecf81f0d5ccdff0ae316b5e62b
Mitch Roote <mitch.roote@esentire.com>
2025-06-05 10:01:24 -0400

    Add mini highlighting config, update neotree

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,16 +11,31 @@ return {
     require('mini.cursorword').setup {}
     require('mini.bufremove').setup {}
     require('mini.comment').setup {}
+
     require('mini.icons').setup()
     MiniIcons.mock_nvim_web_devicons()
-    -- require('mini.notify').setup {}
+
+    local hipatterns = require('mini.hipatterns')
+    hipatterns.setup({
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+        hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+        todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+        note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+    })
+
     local indentscope = require 'mini.indentscope'
     indentscope.setup {
       draw = {
-        delay = 50,
+        delay = 10,
         animation = indentscope.gen_animation.quadratic {
           easing = 'in',
-          duration = 5,
+          duration = 15,
           unit = 'step',
         },
       },
