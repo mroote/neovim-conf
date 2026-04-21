b3ea6ecbee683bc93e569be3f5b37cb1b4aeb2db
Mitch Roote <mitch@r00t.ca>
2026-04-20 21:43:30 -0400

    Migrate nvim-treesitter config to main branch API
    The main branch is an incompatible rewrite targeting Neovim 0.12.
    Replace the legacy configs.setup() call with the new explicit install
    and FileType autocmd approach. Switch nvim-treesitter-textobjects to
    its main branch with vim.keymap.set wiring. Drop incremental_selection
    which is not supported on main.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-local ts_configs = {
+local ensure_installed = {
   'c',
   'cpp',
   'go',
@@ -8,7 +8,6 @@ local ts_configs = {
   'tsx',
   'javascript',
   'typescript',
-  'vimdoc',
   'vim',
   'bash',
   'terraform',
@@ -18,87 +17,90 @@ local ts_configs = {
   'elixir',
   'markdown',
   'markdown_inline',
+  -- 'vimdoc' is installed for the 'help' filetype (mapped below)
+  'vimdoc',
 }
 
-return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  lazy = false,
-  build = ':TSUpdate',
-  config = function()
-    -- [[ Configure Treesitter ]]
-    vim.defer_fn(function()
-      require('nvim-treesitter.configs').setup {
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = ts_configs,
+-- Parser names that don't match their filetype 1:1
+local parser_to_ft = {
+  vimdoc = 'help',
+}
 
-        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-        auto_install = true,
-        -- Install languages synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-        -- List of parsers to ignore installing
-        ignore_install = {},
-        -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-        modules = {},
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<M-space>',
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-            },
-          },
-        },
-      }
-    end, 0)
-  end,
+-- Build the filetype list for the FileType autocmd
+-- markdown_inline is injected; it has no standalone filetype
+local ft_patterns = {}
+for _, parser in ipairs(ensure_installed) do
+  if parser ~= 'markdown_inline' then
+    local ft = parser_to_ft[parser] or parser
+    table.insert(ft_patterns, ft)
+  end
+end
+
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
+    build = ':TSUpdate',
+    config = function()
+      local installed = require('nvim-treesitter.config').get_installed()
+      local to_install = vim.tbl_filter(
+        function(p) return not vim.list_contains(installed, p) end,
+        ensure_installed
+      )
+      if #to_install > 0 then
+        require('nvim-treesitter').install(to_install)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = ft_patterns,
+        callback = function(args)
+          vim.treesitter.start()
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-treesitter-textobjects').setup({
+        select = { lookahead = true },
+        move = { set_jumps = true },
+      })
+
+      local sel  = require('nvim-treesitter-textobjects.select')
+      local mov  = require('nvim-treesitter-textobjects.move')
+      local swap = require('nvim-treesitter-textobjects.swap')
+
+      for lhs, capture in pairs({
+        aa = '@parameter.outer',
+        ia = '@parameter.inner',
+        af = '@function.outer',
+        ['if'] = '@function.inner',
+        ac = '@class.outer',
+        ic = '@class.inner',
+      }) do
+        vim.keymap.set({ 'x', 'o' }, lhs, function()
+          sel.select_textobject(capture, 'textobjects')
+        end)
+      end
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']m',  function() mov.goto_next_start('@function.outer',     'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, ']]',  function() mov.goto_next_start('@class.outer',        'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, ']M',  function() mov.goto_next_end('@function.outer',       'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '][',  function() mov.goto_next_end('@class.outer',          'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[m',  function() mov.goto_previous_start('@function.outer', 'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[[',  function() mov.goto_previous_start('@class.outer',    'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[M',  function() mov.goto_previous_end('@function.outer',   'textobjects') end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[]',  function() mov.goto_previous_end('@class.outer',      'textobjects') end)
+
+      vim.keymap.set('n', '<leader>a', function() swap.swap_next('@parameter.inner')     end)
+      vim.keymap.set('n', '<leader>A', function() swap.swap_previous('@parameter.inner') end)
+    end,
+  },
 }
